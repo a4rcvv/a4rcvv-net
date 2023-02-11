a7d77f84955e812242ef7477e62a6099d82b383b
a4rcvv <35623957+a4rcvv@users.noreply.github.com>
2023-02-12 01:13:48 +0900

    feat: bundleanaliyzerを使えるようにした

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: true
-  }
-}
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
