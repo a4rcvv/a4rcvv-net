ebc8d6c3778597e257f3e2f8ffc8ba28d59db87c
a4rcvv <35623957+a4rcvv@users.noreply.github.com>
2023-05-08 17:56:31 +0900

    デフォルトOGP画像を追加

--- src/lib/MyHead.tsx
+++ src/lib/MyHead.tsx
@@ -15,6 +15,11 @@ export const MyHead = (props: MyHeadProps) => {
   const router = useRouter();
   const origin = process.env.NEXT_PUBLIC_URL;
   const url = origin + router.asPath;
+  const image = props.image ?? {
+    imageUrl: "/images/ogp.jpg",
+    imageWidth: 400,
+    imageHeight: 400,
+  };
   return (
     <Head>
       <title>{props.title}</title>
@@ -25,21 +30,9 @@ export const MyHead = (props: MyHeadProps) => {
       {props.description && (
         <meta property="og:description" content={props.description} />
       )}
-      {props.image && (
-        <meta property="og:image" content={props.image.imageUrl} />
-      )}
-      {props.image && (
-        <meta
-          property="og:image:width"
-          content={props.image.imageWidth.toString()}
-        />
-      )}
-      {props.image && (
-        <meta
-          property="og:image:height"
-          content={props.image.imageHeight.toString()}
-        />
-      )}
+      <meta property="og:image" content={image.imageUrl} />
+      <meta property="og:image:width" content={image.imageWidth.toString()} />
+      <meta property="og:image:height" content={image.imageHeight.toString()} />
       <meta name="twitter:card" content="summary" />
     </Head>
   );
